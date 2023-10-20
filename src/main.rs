13c49e9e3dd7fa2291d79c3fc63fd1054a181bf2
LuciAkirami <ajsubscriber001@gmail.com>
2023-10-20 16:31:35 +0530

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 // Uncomment this block to pass the first stage
+#[allow(unused_imports)]
+use std::io::{prelude::*, BufReader, Read, Write};
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,10 +11,12 @@ fn main() {
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
+    //let mut streamer = TcpStream::connect("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
+                //println!("{:?}");
+                handle_connetions(_stream);
                 println!("accepted new connection");
             }
             Err(e) => {
@@ -19,4 +24,20 @@ fn main() {
             }
         }
     }
+
+    //streamer.read(&mut [0; 128]);
+}
+
+fn handle_connetions(mut stream: TcpStream) {
+    // let buffer = BufReader::new(&mut stream);
+    // let http_request: Vec<_> = buffer
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|result| !result.is_empty())
+    //     .collect();
+    // println!("Request: {http_request:#?}");
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let strin = stream.read_to_string(&mut String::new());
+    println!("{strin:#?}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
